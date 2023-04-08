7d91f5686d783c9ac4917d21241bae6cc69c7487
Dmitriy <snipere@list.ru>
2023-04-08 20:45:26 +1000

    Add delete, edit, done for tasksVC

--- RealmApp/ViewControllers/TasksViewController.swift
+++ RealmApp/ViewControllers/TasksViewController.swift
@@ -60,6 +60,46 @@ class TasksViewController: UITableViewController {
 
 }
 
+// MARK: - Table view delegate
+extension TasksViewController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let task = indexPath.section == 0 ? currentTasks[indexPath.row] : completedTasks[indexPath.row]
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [unowned self] _, _, _ in
+            storageManager.delete(task)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+        
+        let editAction = UIContextualAction(style: .normal, title: "Edit") { [unowned self] _, _, isDone in
+            showAlert(with: task) {
+                tableView.reloadRows(at: [indexPath], with: .automatic)
+            }
+            isDone(true)
+        }
+        
+        let doneAction = UIContextualAction(style: .normal, title: "Done") { [unowned self] _, _, isDone in
+            storageManager.done(task)
+            
+            let section = indexPath.section == 0 ? 1 : 0
+            let tasks = section == 0 ? currentTasks : completedTasks
+            let row = tasks?.index(matching: { $0.title == task.title }) ?? 0
+            let destinationIndexPath = IndexPath(row: row, section: section)
+            
+            tableView.moveRow(at: indexPath, to: destinationIndexPath)
+            isDone(true)
+        }
+        
+        editAction.backgroundColor = .orange
+        doneAction.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        
+        return UISwipeActionsConfiguration(actions: [doneAction, editAction, deleteAction])
+    }
+}
+
 extension TasksViewController {
     private func showAlert(with task: Task? = nil, completion: (() -> Void)? = nil) {
         let taskAlertFactory = TaskAlertControllerFactory(
@@ -69,7 +109,12 @@ extension TasksViewController {
         )
         let alert = taskAlertFactory.createAlert { [weak self] taskTitle, taskNote in
             if let task, let completion {
-                // TODO: - edit task
+                self?.storageManager.edit(
+                    task,
+                    newTitle: taskTitle,
+                    newNote: taskNote
+                )
+                completion()
             } else {
                 self?.save(task: taskTitle, withNote: taskNote)
             }
